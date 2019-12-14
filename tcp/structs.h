2dd47699f3f275a769544de09988a3c53f0a7478
Vladislava Vasileva <vlada318@live.com>
2019-12-14 15:43:48 +0300

    free_message() function added

--- tcp/structs.h
+++ tcp/structs.h
@@ -26,11 +26,10 @@ struct Message {
     int size;
 } typedef Message;
 
-char *concat(char *s1, char *s2) {
-    char *result = malloc(strlen(s1) + strlen(s2) + 1);
-    strcpy(result, s1);
-    strcat(result, s2);
-    return result;
+void free_message(Message *message) {
+    free(message->buffer);
+    free(message);
+
 }
 
 char *cur_time() {
